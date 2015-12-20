c2961a91eace3a24b50c13bd3a04a2cf6ca7066b
Vamshi@Mac <vamshi4001@gmail.com>
2015-12-20 23:07:18 +0530

    Changed map flow to get the centre on map, then initiate the map and then get coordinates from DB in a 5km radius. changed init function from map.html to centerOnMe from init

--- www/js/controllers/MapCtrl.js
+++ www/js/controllers/MapCtrl.js
@@ -3,61 +3,67 @@
  */
 angular.module("oyedelhi")
     .controller('MapCtrl', function ($scope, $ionicLoading, $compile) {
-        $scope.init = function () {
-            var myLatlng = new google.maps.LatLng(12.9272442,77.6660118);
-            var mapOptions = {
-                center: myLatlng,
-                zoom: 13,
-                mapTypeId: google.maps.MapTypeId.ROADMAP
-            };
-            var map = new google.maps.Map(document.getElementById("map"),mapOptions);
-            //Marker + infowindow + angularjs compiled ng-click
-            var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
-            var compiled = $compile(contentString)($scope);
-            var infowindow = new google.maps.InfoWindow({
-                content: compiled[0]
-            });
-            var marker = new google.maps.Marker({
-                position: myLatlng,
-                map: map,
-                title: 'Cinemax Bellandur'
-            });
-            google.maps.event.addListener(marker, 'click', function () {
-                infowindow.open(map, marker);
-            });
-            $scope.map = map;
-            $scope.fetchCoordinates();
-        };
-        $scope.addMarker = function (coord) {
+        $scope.currentLocation = null
+
+        // centerOnMe -> initiateMap -> fetchCoordinates n map them
+
+        $scope.addMarker = function (coord, icon) {
             var marker = new google.maps.Marker({
                 position: coord,
                 map: $scope.map,
                 title: 'Hello World!'
             });
         };
+        $scope.initiateMap = function(centerCoords){
+            var mapOptions = {
+                center: centerCoords,
+                zoom: 13,
+                mapTypeId: google.maps.MapTypeId.ROADMAP
+            };
+            var map = new google.maps.Map(document.getElementById("map"),mapOptions);
+            //Infowindow and Angular compile 
+            // var contentString = "<div><a ng-click='clickTest()'>Click me!</a></div>";
+            // var compiled = $compile(contentString)($scope);
+            // var infowindow = new google.maps.InfoWindow({
+            //     content: compiled[0]
+            // });
+            // var marker = new google.maps.Marker({
+            //     position: myLatlng,
+            //     map: map,
+            //     title: 'Your location'
+            // });
+            // google.maps.event.addListener(marker, 'click', function () {
+            //     infowindow.open(map, marker);
+            // });
+            $scope.map = map;
+            $scope.map.setCenter(centerCoords);
+            $ionicLoading.hide()
+            $scope.addMarker({lat: centerCoords.lat(), lng: centerCoords.lng()}, "icon");
+            $scope.fetchCoordinates();
+        }
         $scope.centerOnMe = function () {
-            if (!$scope.map) {
-                return;
-            }
             $ionicLoading.show()
             var options = {timeout: 3000, enableHighAccuracy: true, maximumAge: 10000};
             navigator.geolocation.getCurrentPosition(function (pos) {
-                $scope.map.setCenter(new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude));
-                console.log(pos);
-                alert(JSON.stringify(pos));
-                $ionicLoading.hide()
-
-                $scope.addMarker({lat: pos.coords.latitude, lng: pos.coords.longitude});
-
+                $scope.currentLocation = new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude)
+                $scope.initiateMap($scope.currentLocation);                
             }, function (error) {
                 $ionicLoading.hide()                
-                alert('Unable to get location: ' + error.message);
+                navigator.notification.alert(
+                    'Unable to get location!',
+                    $scope.locationFallback(),         
+                    'Location', 
+                    'Okay'
+                );
             }, options);
         };
-        $scope.fetchCoordinates = function(){
+        $scope.locationFallback = function(){
+            $scope.initiateMap(new google.maps.LatLng(28.6540471,77.1732288));            
+        }
+        $scope.fetchCoordinates = function(){            
             var vehicleInfo = Parse.Object.extend("vehicleInfo");
             var vehicle = new Parse.Query(vehicleInfo);
-            var location = new Parse.GeoPoint($scope.map.getCenter().lat(), $scope.map.getCenter().lng());
+            var location = new Parse.GeoPoint($scope.currentLocation.lat(), $scope.currentLocation.lng());
             vehicle.withinKilometers("location", location, 5);
             vehicle.find({
                 success: function(objects) {                    
@@ -67,7 +73,7 @@ angular.module("oyedelhi")
                     $.each(objects, function(i,v){
                         $scope.locations.push(new google.maps.LatLng(v.attributes.location._latitude, v.attributes.location._longitude));
                         $scope.addMarker(new google.maps.LatLng(v.attributes.location._latitude, v.attributes.location._longitude))                        
-                    })                          
+                    })                      
                 },
                 error: function(error) {
                     console.log("An error occured :(");
